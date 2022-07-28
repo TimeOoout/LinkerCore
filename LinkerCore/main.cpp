1f10b09d061c79951874e10a543ea4c80e5bc243
Hang Jiayu <88521229+Hang-Jiayu@users.noreply.github.com>
2022-07-28 16:05:08 +0800

    清理&整理

--- LinkerCore/main.cpp
+++ LinkerCore/main.cpp
@@ -11,17 +11,6 @@ int main(int argc, char* argv[])
 	Linker_pro.init();
 	LC.init_settings(Linker_pro.Settings);
 	qDebug() << QString::fromLocal8Bit("³õÊ¼»¯:") << LC.init(Linker_pro.MainPath) << endl;
-	/*qDebug() << QString::fromLocal8Bit("²âÊÔ»ñÈ¡ÉèÖÃ:") << LC.get_setting("Project") << endl;
-	qDebug() << QString::fromLocal8Bit("²âÊÔ×¢²á:" )<< LC.reg("²âÊÔ") << endl;
-	qDebug() << QString::fromLocal8Bit("²âÊÔµÇÂ¼:") << LC.login("²âÊÔ") << endl;
-	qDebug() << QString::fromLocal8Bit("²âÊÔ´´½¨ÓÃ»§×é'ÆÕÍ¨':") << LC.create_usergroup("ÆÕÍ¨") << endl;
-	qDebug() << QString::fromLocal8Bit("²âÊÔÌí¼ÓÓÃ»§µ½ÓÃ»§×é'ÆÕÍ¨':") << LC.add_user("²âÊÔ", "ÆÕÍ¨") << endl;
-	qDebug() << LC.get_groupusers("ÆÕÍ¨");
-	qDebug() << LC.get_usergroups("²âÊÔ") << endl;
-	qDebug() << QString::fromLocal8Bit("²âÊÔÉ¾³ýÓÃ»§'²âÊÔ':") << LC.delete_user("²âÊÔ") << endl;
-	qDebug() << QString::fromLocal8Bit("²âÊÔ×éÄÚÉ¾³ýÓÃ»§'²âÊÔ':") << LC.remove_user("²âÊÔ","ÆÕÍ¨") << endl;*/
-
-
 	LC.write_log("³õÊ¼»¯:" + QString::number(LC.inited_result).toStdString());
 	LC.write_log("²âÊÔ×¢²á:" + QString::number(LC.reg("²âÊÔ")).toStdString());
 	LC.write_log("²âÊÔµÇÂ¼:" + QString::number(LC.login("²âÊÔ")).toStdString());
